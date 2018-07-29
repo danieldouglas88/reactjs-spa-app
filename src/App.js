8b118a05f33a5454f1c637adedc066dc94c635c7
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-07-28 22:31:41 -0700

    init
    commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+      return(
+      function home(){
+           <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+                  <h1 className="App-title">Welcome there</h1>
+                  <h1 className="App-title">Welcome there</h1>
+                  <h1 className="App-title">Welcome there</h1>
+        </p>
+      </div>
+      } );
+      return(
+            function about(){
+           <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to FUCK</h1>
+        </header>
+        <p className="App-intro">
+                  <h1 className="App-title">fuck off</h1>
+                  <h1 className="App-title">fuck off</h1>
+                  <h1 className="App-title">fuck off</h1>
+        </p>
+      </div>
+      });
+  }
+}
+
+export default App;
